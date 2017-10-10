1119058dedd36e8490474596fb29d31585ca601f
Emily Legein <emilylegein@scarlet.be>
2017-10-10 22:05:17 +0200

    portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
 
-
+// const clickButton = () => {
+//   console.log(`click`);
+//
+//   // window.scrollTo(0, document.querySelector(`.who`).scrollHeight);
+//   document.documentElement.scrollTop = document.body.scrollTop = 803;
+//
+// };
+//
+//
 const init = () => {
 
-  console.log(`Hello, portfolio`)
+  console.log(`Hello, portfolio`);
 
+  // document.getElementById(`myBtn`).addEventListener(`click`, clickButton);
 };
 
 init();
